329319e52b654f776d1775eabee7ac4dd883a99e
Shantini Persaud <shantini@gmail.com>
2018-04-02 11:44:56 -0700

    debugging input view

--- Pawsy/View Controllers/Onboarding View Controllers/InputViewController.swift
+++ Pawsy/View Controllers/Onboarding View Controllers/InputViewController.swift
@@ -33,8 +33,6 @@ class InputViewController: UIViewController, BreedViewControllerDelegate, UIImag
     @IBOutlet weak var yesButton: DLRadioButton!
 
     var currentInput: Int = 0
-    var inputImages = [UIImage]()
-    var itemList: [UIView]?
     var breed: String?
     var alertController: UIAlertController = UIAlertController()
     var photoPicker: UIImagePickerController = UIImagePickerController()
@@ -78,127 +76,28 @@ class InputViewController: UIViewController, BreedViewControllerDelegate, UIImag
 
     //cycle through screen images until last screen reached
     @IBAction func next(_ sender: Any) {
-        if  selectPhoto.isHidden == false {
-            let alert = UIAlertController(title: "No Image Selected", message: "Please select an image to upload", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
-                alert.dismiss(animated: true, completion: nil)
-            }))
-            self.present(alert, animated: true, completion: nil)
-        }
-        else if (Name.text?.count)! < 1 && Name.isHidden == false {
-                print("is Nil")
-                let alert = UIAlertController(title: "No Name Entered", message: "Please enter a name.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
-                    alert.dismiss(animated: true, completion: nil)
-                }))
-                self.present(alert, animated: true, completion: nil)
-        }
-            
-        else if (Weight.text?.count)! < 1 && Weight.isHidden == false {
-                let alert = UIAlertController(title: "No Weight Entered", message: "Please enter a number for weight.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
-                    alert.dismiss(animated: true, completion: nil)
-                }))
-                self.present(alert, animated: true, completion: nil)
-        }
-            
-        else if Gender.isHidden == false && self.dogGender == nil{
-            
-            let alert = UIAlertController(title: "No Gender Selected", message: "Please select a gender.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
-                alert.dismiss(animated: true, completion: nil)
-            }))
-            self.present(alert, animated: true, completion: nil)
-            
-        }
-        else if Fixed.isHidden == false && self.dogFixed == nil{
-            
-            let alert = UIAlertController(title: "No Spay/Neuter Status Selected", message: "Please select an option.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
-                alert.dismiss(animated: true, completion: nil)
-            }))
-            self.present(alert, animated: true, completion: nil)
-        }
-       
-        else if self.breed == nil && selectBreed.isHidden == false {
-                let alert = UIAlertController(title: "No Breed Selected", message: "Please select a breed.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
-                    alert.dismiss(animated: true, completion: nil)
-                }))
-                self.present(alert, animated: true, completion: nil)
-        }
-        else if currentInput < inputImages.count {
-            self.cycleImages()
-        }
-        
-        else {
-            self.nextButton.isHidden = true
-            self.inputImageView.isHidden = true
-            self.profilePhoto.isHidden = true
-            let newFrame = CGRect(x: view.frame.width/2-100, y: view.frame.height/2-100, width: 200, height: 200)
-            let lottieView = LOTAnimationView(name: "loading")
-            lottieView.frame = newFrame
-            lottieView.loopAnimation = true
-            self.view.addSubview(lottieView)
-            lottieView.play()
-            self.uploadToCloudinary(photo: self.photo!)
-            
-        }
+        self.manageViews()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.profilePhoto.clipsToBounds = true
+        self.profilePhoto.layer.cornerRadius = self.profilePhoto.frame.size.width / 2
+    }
+
+    override func viewDidLoad() {
+        hideKeyboard()
+        setUpAlertController()
+        photoPicker.delegate = self
+        self.cloudinary = CLDCloudinary(configuration: self.config!)
+        self.boyButton.otherButtons = [self.girlButton, self.neutralButton]
+        self.boyButton.isMultipleSelectionEnabled = false
+        self.yesButton.otherButtons = [self.noButton]
+        self.yesButton.isMultipleSelectionEnabled = false
+        manageViews()
     }
     
-    //delegate method for breed selector
-    func goBack(breed: String) {
-        self.breed = breed
-        self.breedLabel.text = breed
-        self.breedLabel.isHidden = false
-    }
-    
-    func cycleImages(){
-        print ("cycling")
-        self.breedLabel.isHidden = true
-        if self.selectBreed.isHidden == false{
-            self.selectBreed.isHidden = true
-        }
-        if self.Name.isHidden == false{
-            self.Name.isHidden = true
-        }
-        if self.Weight.isHidden == false{
-            self.Weight.isHidden = true
-        }
-        if self.Fixed.isHidden == false{
-            self.Fixed.isHidden = true
-        }
-        if self.Gender.isHidden == false{
-            self.Gender.isHidden = true
-        }
-        if self.agePicker.isHidden == false{
-            self.agePicker.isHidden = true
-        }
-        if self.selectPhoto.isHidden == false{
-            self.selectPhoto.isHidden = true
-        }
-        if self.profilePhoto.isHidden == false{
-            self.profilePhoto.isHidden = true
-        }
-        currentInput += 1
-        inputImageView.image = inputImages[currentInput]
-        itemList![currentInput].isHidden = false
-        itemList![currentInput].center.x = view.center.x
-    }
-    
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        picker.dismiss(animated: true, completion: nil)
-        let image = info["UIImagePickerControllerOriginalImage"] as! UIImage
-        self.photo = UIImageJPEGRepresentation(image, 1)!
-        self.profilePhoto.isHidden = false
-        self.profilePhoto.image = image
-        self.profilePhoto.layer.masksToBounds = true
-        self.profilePhoto.frame = CGRect(x: 87, y: 200, width: 200, height: 200)
-        self.profilePhoto.layer.cornerRadius = self.profilePhoto.frame.width/2
-        
-    }
-    
+
+
     func setUpAlertController(){
         alertController.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { (alertAction) in
             self.photoPicker.sourceType = .camera
@@ -209,28 +108,153 @@ class InputViewController: UIViewController, BreedViewControllerDelegate, UIImag
             self.present(self.photoPicker, animated: true, completion: nil)
         }))
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
+
     }
-    func setUpImages(){
-        self.inputImages.append(UIImage(named:"NamePage")!)
-        self.inputImages.append(UIImage(named:"WeightPage")!)
-        self.inputImages.append(UIImage(named:"BirthdatePage")!)
-        self.inputImages.append(UIImage(named:"GenderPage")!)
-        self.inputImages.append(UIImage(named:"FixPage")!)
-        self.inputImages.append(UIImage(named:"BreedPage")!)
-        self.inputImages.append(UIImage(named:"PicPage")!)
-        self.inputImageView.image = self.inputImages[0]
-        itemList = [Name, Weight, agePicker, Gender, Fixed, selectBreed, selectPhoto]
-        agePicker.setValue(UIColor.black, forKeyPath: "textColor")
-        agePicker.backgroundColor = UIColor.white
-        itemList![0].isHidden = false
-        itemList![0].center = view.center
-        self.profilePhoto.frame = CGRect(x: 0, y:0 , width: 200, height: 200)
-        self.profilePhoto.contentMode = .scaleAspectFit
-        self.profilePhoto.center = view.center
-        
+
+    func manageViews(){
+        switch self.currentInput {
+        case 0:
+            self.Name.isHidden = false
+            self.inputImageView.image = UIImage(named:"NamePage")!
+            self.currentInput += 1
+            return
+        case 1:
+            if (Name.text?.count)! < 1 {
+                print("is Nil")
+                let alert = UIAlertController(title: "No Name Entered", message: "Please enter a name.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            else{
+                self.Weight.isHidden = false
+                self.Name.isHidden = true
+                self.inputImageView.image = UIImage(named:"WeightPage")!
+                self.currentInput += 1
+                return
+            }
+        case 2:
+            if (Weight.text?.count)! < 1 {
+                let alert = UIAlertController(title: "No Weight Entered", message: "Please enter a number for weight.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            else{
+                self.Weight.isHidden = true
+                self.agePicker.isHidden = false
+                agePicker.setValue(UIColor.black, forKeyPath: "textColor")
+                agePicker.backgroundColor = UIColor.white
+                self.inputImageView.image = UIImage(named:"BirthdatePage")!
+                self.currentInput += 1
+                return
+            }
+        case 3:
+            self.agePicker.isHidden = true
+            self.Gender.isHidden = false
+            self.inputImageView.image = UIImage(named:"GenderPage")!
+            self.currentInput += 1
+            return
+        case 4:
+            if  self.dogGender == nil{
+                let alert = UIAlertController(title: "No Gender Selected", message: "Please select a gender.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            else{
+                self.Gender.isHidden = true
+                self.Fixed.isHidden = false
+                self.inputImageView.image = UIImage(named:"FixPage")!
+                self.currentInput += 1
+                return
+            }
+
+        case 5:
+            if self.dogFixed == nil{
+                let alert = UIAlertController(title: "No Spay/Neuter Status Selected", message: "Please select an option.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            else{
+                self.Fixed.isHidden = true
+                self.selectBreed.isHidden = false
+                self.inputImageView.image = UIImage(named:"BreedPage")!
+                self.currentInput += 1
+                return
+            }
+
+        case 6:
+            if self.breed == nil  {
+                let alert = UIAlertController(title: "No Breed Selected", message: "Please select a breed.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            else{
+                self.selectBreed.isHidden = true
+                self.breedLabel.isHidden = true
+                self.selectPhoto.isHidden = false
+                self.inputImageView.image = UIImage(named:"PicPage")!
+                self.currentInput += 1
+                return
+            }
+
+        case 7:
+            if  self.profilePhoto == nil {
+                let alert = UIAlertController(title: "No Image Selected", message: "Please select an image to upload", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (uialert) in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            else{
+                self.selectPhoto.isHidden = true
+                self.profilePhoto.isHidden = true
+                self.inputImageView.isHidden = true
+                self.nextButton.isHidden = true
+                let newFrame = CGRect(x: view.frame.width/2-100, y: view.frame.height/2-100, width: 200, height: 200)
+                let lottieView = LOTAnimationView(name: "loading")
+                lottieView.frame = newFrame
+                lottieView.loopAnimation = true
+                self.view.addSubview(lottieView)
+                lottieView.play()
+                self.uploadToCloudinary(photo: self.photo!)
+                return
+            }
+        default:
+            return
+        }
+    }
+
+    //delegate method for breed selector
+    func goBack(breed: String) {
+        self.breed = breed
+        self.breedLabel.text = breed
+        self.breedLabel.isHidden = false
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let image = info["UIImagePickerControllerOriginalImage"] as! UIImage
+        self.photo = UIImageJPEGRepresentation(image, 1)!
+        self.profilePhoto.image = image
+        self.profilePhoto.isHidden = false
+
+        
+    }
     func uploadToCloudinary(photo: Data) {
         
         _ = self.cloudinary?.createUploader().upload(data: photo, uploadPreset: "pawsyDogPic", params: nil, progress: {({ (progress) in
@@ -245,7 +269,6 @@ class InputViewController: UIViewController, BreedViewControllerDelegate, UIImag
             }
         })
     }
-
     
     func uploadToFirebase(photoURL: String){
         let db = Firestore.firestore()
@@ -262,7 +285,6 @@ class InputViewController: UIViewController, BreedViewControllerDelegate, UIImag
         if self.locationManager.location != nil {
             dogDoc?.updateData(["longitude":self.locationManager.location!.coordinate.longitude, "latitude":self.locationManager.location!.coordinate.latitude])
         }
-        
 
         let userDoc = db.collection("users").document(self.user.uid)
         userDoc.collection("dogs").addDocument(data: ["dogID": dogDoc?.documentID])
@@ -275,29 +297,10 @@ class InputViewController: UIViewController, BreedViewControllerDelegate, UIImag
         
     }
 
-
-
-    
-    override func viewDidLoad() {
-        setUpImages()
-        hideKeyboard()
-        setUpAlertController()
-        photoPicker.delegate = self
-        self.cloudinary = CLDCloudinary(configuration: self.config!)
-        self.boyButton.otherButtons = [self.girlButton, self.neutralButton]
-        self.boyButton.isMultipleSelectionEnabled = false
-        self.yesButton.otherButtons = [self.noButton]
-        self.yesButton.isMultipleSelectionEnabled = false
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    
-    }
-
 
 }
 
